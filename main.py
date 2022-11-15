7f794b53376e9fbb0156e29f98e52382bb5e2c10
Jorge-E-Cadena <jecdapde@gmail.com>
2022-11-14 23:22:47 -0600

    Created a main file with some of the musical genres that I could think of

---
+++ main.py
@@ -0,0 +1,14 @@
+musicalGenres = {
+    'Regional Mexicano': [
+        'Joan Sebastian',
+        'Ángela Aguilar',
+        'Alejandro Fernández',
+        'Ana Bárbara',
+        'Juan Gabriel',
+        'Pedro Infante',
+        'Vicente Fernández',
+        'Yuridia',
+        'Jenni Rivera',
+        'Los Tucanes de Tijuana'
+    ]
+}
